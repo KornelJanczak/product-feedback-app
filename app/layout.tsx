2998389e19186b10d7f81024c71ece55870ef465
KornelJanczak <korneljanczak10@gmail.com>
2024-02-05 00:22:53 +0100

    fix: try fix auth bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,8 @@ export const metadata: Metadata = {
   description: "Create your own section!",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: Readonly<{
