3841fbce9e3cb0f10ca4eefdbe00184707e51830
KornelJanczak <korneljanczak10@gmail.com>
2024-02-04 14:02:25 +0100

    refactor

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -11,6 +11,7 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
   pages: {
     signIn: "/login",
   },
