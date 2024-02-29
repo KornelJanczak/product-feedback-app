5c8e245d8381037133c20fd9422e183f74077c34
KornelJanczak <korneljanczak10@gmail.com>
2024-02-29 23:02:23 +0100

    build: filter-bar for home page

--- app/(dashboard)/(home)/_components/filter-bar-select.tsx
+++ app/(dashboard)/(home)/_components/filter-bar-select.tsx
@@ -9,21 +9,35 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Separator } from "@radix-ui/react-select";
 
 export function FilterBarSelect() {
   return (
     <Select>
-      <SelectTrigger className="w-[180px] bg-dark border-dark text-middleWhite focus-visible:ring-0">
-        <SelectValue placeholder="Select a fruit" />
+      <SelectTrigger
+        className="w-[180px] 
+      bg-dark border-dark text-darkWhite
+      px-2
+      focus:ring-0 focus:ring-ring focus:ring-offset-0
+      gap-0
+        "
+      >
+        <SelectValue placeholder="Sort by" />
       </SelectTrigger>
-      <SelectContent className="bg-dark focus-visible:ring-0">
-        <SelectGroup className="bg-dark focus-visible:ring-0">
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
+      <SelectContent className="bg-darkWhite focus-visible:ring-0 border-0 focus:ring-0">
+        <SelectGroup className="bg-darkWhite text-grey focus-visible:ring-0  ">
+          <SelectItem value="Most Suggestions" className="hover:text-pink">
+            Most Suggestions
+          </SelectItem>
+          <SelectItem value="Least Suggestions" className="hover:text-pink">
+            Least Suggestions
+          </SelectItem>
+          <SelectItem value="Most members" className="hover:text-pink">
+            Most members
+          </SelectItem>
+          <SelectItem value="Least members" className="hover:text-pink">
+            Least members
+          </SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
--- app/(dashboard)/(home)/_components/filter-bar.tsx
+++ app/(dashboard)/(home)/_components/filter-bar.tsx
@@ -5,18 +5,34 @@ import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { FilterBarSelect } from "./filter-bar-select";
 import SearchIcon from "@/public/icons/search";
+import { Input } from "@/components/ui/input";
 
 export default function FilterBar({ className }: { className?: string }) {
   const router = useRouter();
   return (
     <form
       className={cn(
-        "flex w-full items-center gap-2 py-3 px-3 bg-dark md:rounded-lg md:container",
+        "flex w-full items-center gap-2 py-3 px-5 bg-dark md:rounded-lg ",
         className
       )}
     >
       <Button
-        className=""
+        type="submit"
+        className={
+          "flex gap-1 bg-blue hover:opacity-60 hover:bg-blue hover:transition duration-300 mt-0 lg:mt-0"
+        }
+      >
+        <span className="hidden sm:block">Search</span>
+        <SearchIcon />
+      </Button>
+      <Input
+        className="hidden sm:block sm:w-8/12 lg:w-6/12 xl:w-4/12 bg-transparent border-0 border-b-2 focus-visible:ring-offset-0 focus-visible:ring-0
+         border-darkWhite bg-darkWhite w-auto text-dark placeholder:text-dark"
+        placeholder="Search section"
+      />
+      <FilterBarSelect />
+      <Button
+        className="bg-pink hover:opacity-60 hover:bg-pink hover:transition duration-300 ml-auto"
         type="button"
         onClick={() => {
           router.push("/add-section");
@@ -24,16 +40,6 @@ export default function FilterBar({ className }: { className?: string }) {
       >
         <PlusIcon />
         Add Section
-      </Button>
-      <FilterBarSelect />
-      <Button
-        type="submit"
-        className={
-          "flex gap-1 w-4/12 bg-pink hover:opacity-60 hover:bg-pink hover:transition duration-300 mt-0  lg:mt-0"
-        }
-      >
-        Search
-        <SearchIcon />
       </Button>
     </form>
   );
