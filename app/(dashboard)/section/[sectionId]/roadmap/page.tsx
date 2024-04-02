91ed4ac7acef6b10e427eb51268890b7a5ddf02b
KornelJanczak <korneljanczak10@gmail.com>
2024-04-02 22:02:43 +0200

    build: roadmap page

--- app/(dashboard)/section/[sectionId]/roadmap/page.tsx
+++ app/(dashboard)/section/[sectionId]/roadmap/page.tsx
@@ -1,3 +1,28 @@
+import BackButton from "@/components/back-button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function RoadMapPage() {
-  return <div>aaaaa</div>;
+  return (
+    <main>
+      <div className="flex justify-between items-center px-5 py-4 bg-secondDark">
+        <div className="flex flex-col gap-2">
+          <BackButton />
+          <h2 className="text-basicWhite font-semibold pl-3">Roadmap</h2>
+        </div>
+        <button>add </button>
+      </div>
+      <Tabs defaultValue="in-progress" className="w-full">
+        <TabsList className="w-full items-center justify-around">
+          <TabsTrigger value="planned">Planned</TabsTrigger>
+          <TabsTrigger value="in-progress">In-Progress</TabsTrigger>
+          <TabsTrigger value="live">Live</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+      aaaaa
+    </main>
+  );
 }
